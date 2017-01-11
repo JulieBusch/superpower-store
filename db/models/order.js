71cda37ea68291dd14d6cec65b5d4e9d4a6c54fd
jeunlee <junniesu@gmail.com>
2017-01-11 13:58:43 -0500

    started building orders models

---
+++ db/models/order.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Order = db.define('order', {
+  status: ,
+
+
+})
---
+++ db/models/orderline.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Orderline = db.define('orderline', {
+  quantity: ,
+  unitSalePrice: ,
+
+})
