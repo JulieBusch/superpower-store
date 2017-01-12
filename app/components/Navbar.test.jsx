c60b8a2255d826e48435b410b8ca8271d8b60e99
Ya-wen Alice <yawenalice@gmail.com>
2017-01-12 11:17:32 -0500

    navbar testing

--- app/components/Navbar.test.jsx
+++ app/components/Navbar.test.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import chai, {expect} from 'chai'
 chai.use(require('chai-enzyme')())
-import {shallow} from 'enzyme'
-import {spy} from 'sinon'
+import {mount, shallow} from 'enzyme'
+import {spy, stub} from 'sinon'
 chai.use(require('sinon-chai'))
 
-import {Navbar} from './Navbar'
+import NavbarContainer, {Navbar} from './Navbar'
 import {Login} from './Login'
 import {Link} from 'react-router'
+import {createStore} from 'redux'
+
 
 describe('<Navbar />', () => {
+
   let wrapper
-  beforeEach('render the Navbar wrapper', () =>
+  beforeEach('render the Navbar wrapper', () => {
     wrapper = shallow(<Navbar />)
-  )
+  })
 
   it('shows signup and login links when there is no currentUser', () => {
     expect(wrapper.find('.login-ul').childAt(0)).to.have.html('<li><a>signup</a></li>')
@@ -24,10 +27,19 @@ describe('<Navbar />', () => {
   it('shows the login component when login link is clicked', () => {
     wrapper.setState({clicked: true})
 
-    expect(wrapper.contains(
-                            )).to.equal(true)
-    // expect(wrapper.find('input[name="username"]')).to.have.length(1)
-    // expect(wrapper.find('input[name="password"]')).to.have.length(1)
+    expect(wrapper.find('#login')).to.have.length(1)
+  })
+
+  it('gets prop.currentUser from state.auth', () => {
+    const state = {
+      auth: {name: 'Johnny'}
+    }
+    let store, wrapper1, dispatch
+    store = createStore(state => state, state)
+    dispatch = stub(store, 'dispatch')
+    wrapper1 = shallow(<NavbarContainer store={store} />)
+
+    expect(wrapper1.find(Navbar)).to.have.prop('currentUser').eql(state.auth)
   })
 
   //check that the props passed off the store state works
