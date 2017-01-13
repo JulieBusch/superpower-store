58af0e4442d9a7755c25216abc6b493d3c41a459
Julie Busch <heyjuliebusch@gmail.com>
2017-01-13 15:38:29 -0500

    finished single item view redux

--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
 const initialProductsState = {
-	products: []
+	products: [],
+	selectedProduct: {}
 }
+
+/*----------------REDUCER------------------*/
 
 const reducer = (state = initialProductsState, action) => {
 
@@ -11,6 +14,10 @@ const reducer = (state = initialProductsState, action) => {
 	switch(action.type){
 		case RECEIVE_ALL_PRODUCTS:
 		newState.products = action.products;
+		break
+
+		case RECEIVE_SINGLE_PRODUCT:
+		newState.selectedProduct = action.product;
 		break;
 
 		default:
@@ -20,18 +27,34 @@ const reducer = (state = initialProductsState, action) => {
 	return newState
 }
 
+/*----------------ACTION CREATORS------------------*/
+
 const RECEIVE_ALL_PRODUCTS = 'RECEIVE_ALL_PRODUCTS'
+const RECEIVE_SINGLE_PRODUCT = 'RECEIVE_SINGLE_PRODUCT'
 
 export const allProducts = products => ({
 	type: RECEIVE_ALL_PRODUCTS, products
 })
 
+export const singleProduct = product => ({
+	type: RECEIVE_SINGLE_PRODUCT, product
+})
+
+/*----------------AJAX REQUESTS------------------*/
+
 export const receiveAllProducts = () =>
 	dispatch =>
-		axios.get('/api/products')
+		axios.get('/api/products/')
 			.then(res => res.data)
 			.then((foundProducts) => dispatch(allProducts(foundProducts)))
 			.catch((failed) => dispatch(allProducts(["products dispatch failure"])))
+
+export const receiveSingleProduct = (productId) =>
+	dispatch =>
+		axios.get(`/api/products/${productId}`)
+			.then(res => res.data)
+			.then((foundProduct) => dispatch(singleProduct(foundProduct)))
+			.catch((failed) => dispatch(singleProduct({note: "failure"})))
 
 
 
