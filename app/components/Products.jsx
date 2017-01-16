40a0fb9c6994fd61d91637e276e59b2c85f6ec54
jeunlee <junniesu@gmail.com>
2017-01-16 18:32:28 -0500

    merging w master...

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-
-import Splash from './Splash'
-
 import {Link} from 'react-router'
+
 import Splash from './Splash'
 import SingleProduct from './SingleProduct'
 
@@ -15,7 +13,6 @@ export class Products extends Component {
 
   constructor(props) {
     super(props)
-
     // this.handleClick = this.handleClick.bind(this)
   }
 
@@ -26,20 +23,20 @@ export class Products extends Component {
 
   render() {
 
-
     var productDivs = this.props.products.map((product) => {
-          return(
-            <div key={product.id} className="column-3 catalog-tile">
-              <h4>{product.name}</h4>
-              <div className="product-thumbnail">
-                <Link to='#' onClick={this.handleClick.bind(this, product.id)}>
-                  <img src={product.thumbnail} />
-                </Link>
-              </div>
-          )
-      });
+      return (
+        <div key={product.id} className="column-3 catalog-tile">
+          <h4>{product.name}</h4>
+          <div className="product-thumbnail">
+            <Link to='#' onClick={this.handleClick.bind(this, product.id)}>
+              <img src={product.thumbnail} />
+            </Link>
+          </div>
+        </div>
+      )
+    })
 
-    return(
+    return (
       <div>
         <Splash />
   	    <div className="container catalog">
@@ -51,12 +48,11 @@ export class Products extends Component {
       </div>
 		)
 	}
+
 }
 
 
-/* -----------------    CONTAINER     ------------------ */
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     products: state.products.products,
     similarProducts: state.products.similarProducts,
