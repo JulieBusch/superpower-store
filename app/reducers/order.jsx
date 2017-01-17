5fe3e28f31d18ead455fd530a72f9d81ba132291
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-16 19:00:21 -0500

    getting data to render

--- app/reducers/order.jsx
+++ app/reducers/order.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import Order from '../../db/models/order'
+// import Order from '../../db/models/order'
 
 const initialState = {
    orders: [],
@@ -142,22 +142,22 @@ export const updateOrderUser = (order) =>
          .then(updatedOrder => dispatch(updatedOrderUser(updatedOrder)))
          .catch((failed) => console.log(failed))
 
-export const updateOrder = (order) =>
-   dispatch =>
-      axios.put(`/api/orders/${order.id}/product/${order.product_id}`)
-         .then( () => {
-            Order.findById(order.id)
-         })
-         .then(updatedOrder => dispatch(updatedOrderProducts(updatedOrder)))
-         .catch((failed) => console.log(failed))
+// export const updateOrder = (order) =>
+//    dispatch =>
+//       axios.put(`/api/orders/${order.id}/product/${order.product_id}`)
+//          .then( () => {
+//             Order.findById(order.id)
+//          })
+//          .then(updatedOrder => dispatch(updatedOrderProducts(updatedOrder)))
+//          .catch((failed) => console.log(failed))
 
-export const deleteProductFromOrder = (order) =>
-   dispatch =>
-      axios.delete(`/api/orders/${order.id}/product/${order.product_id}`)
-         .then( () => {
-            Order.findById(order.id)
-         })
-         .then(updatedOrder => dispatch(updatedOrderProducts(updatedOrder)))
-         .catch((failed) => console.log(failed))
+// export const deleteProductFromOrder = (order) =>
+//    dispatch =>
+//       axios.delete(`/api/orders/${order.id}/product/${order.product_id}`)
+//          .then( () => {
+//             Order.findById(order.id)
+//          })
+//          .then(updatedOrder => dispatch(updatedOrderProducts(updatedOrder)))
+//          .catch((failed) => console.log(failed))
 
 export default reducer
