a8c1816bd0a03a5534aafe3597924d07f8cdd051
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-17 14:30:53 -0500

    styling cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -26,45 +26,60 @@ class Cart extends React.Component {
 
   render() {
 
+    const convertNum = (str) => {
+      let newStr = str.slice(str.length-6);
+      str = str.slice(0, str.length-6);
+      let count = 0;
+      for(var i=str.length; i>=0; i-- ){
+        newStr = str.charAt(i) + newStr;
+        if(count%3===0 && i>0) newStr = ',' + newStr;
+        count++;
+      }
+      return newStr;
+    }
+
 
     var orderDivs = this.props.orderDetails.map((item) => {
       return (
 
-        <div key={item.id}>
+        <div key={item.id} className="row">
 
-          <div className="nav-left">
-            <div className="item-thumbnail"><img src={item.thumbnail} /></div>
+          <div className="cart-column-4">
+            <div className="item-thumbnail"><img className="cart-thumbnail" src={item.thumbnail} /></div>
             <h4>{item.name}</h4>
           </div>
 
-          <div className="nav-right">
-            <h3>price: {item.price}</h3>
+          <div className="cart-column-2">
+            <h3>price: {convertNum(item.price)}</h3>
             <h3>quantity: {item.orderlines.quantity}</h3>
-            <h3>subtotal: {item.orderlines.subtotal}</h3>
+            <h3>subtotal: {convertNum(item.orderlines.subtotal + ".00")}</h3>
+          </div>
 
-            <img src={'/70287.png'} name={item.id} onClick={this.handleClick} />
-
+          <div className="cart-column-4">
+            <img className="delete-cart-item" src={'/70287.png'} name={item.id} onClick={this.handleClick} />
           </div>
         </div>
         )
     })
 
     return (
-      <div className="container">
+      <div className="container cart">
         <h1> Your Cart </h1>
 
             {!this.props.orderDetails.length && <div>Your shopping cart is empty!</div>}
 
         {orderDivs}
-        <div>
-          Total: {this.props.order.total}
+        <div className="cart-total-column">
+          <div className="total">
+            Total: {this.props.order.total}
+          </div>
+          <div className="total">
+            <Link to="/checkout">
+            Checkout
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to="/checkout">
-          Checkout
-          </Link>
-        </div>
-
+        <div></div>
 
       </div>
     )
