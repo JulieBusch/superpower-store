df9036ff5f0c957f8d444114b23711ec7b78446e
jeunlee <junniesu@gmail.com>
2017-01-17 13:02:45 -0500

    working on prod detail view

--- app/components/SingleProduct.jsx
+++ app/components/SingleProduct.jsx
@@ -32,31 +32,40 @@ export class SingleProduct extends React.Component {
 
   render() {
     var selectedProduct = this.props.selectedProduct
-    console.log("TAAAAAAGS ",selectedProduct.tags)
-    return(
+    return (
       <div className="popUp">
-        <div className="column-2">
-          <Link to="#" onClick={this.handleCloseClick} >Close</Link>
-          <img src={selectedProduct.image} />
-        </div>
-        <div className="column-2">
-          <h2>{selectedProduct.name}</h2>
-          <p>{selectedProduct.description}</p>
+        <div className="row">
+
           <div className="column-2">
-            <span>{selectedProduct.price}</span>
+            <Link to="#" onClick={this.handleCloseClick} >Close</Link>
+            <img src={selectedProduct.image} />
           </div>
+
           <div className="column-2">
-            <button className="product-view-btns" onClick={this.handleAddItemToCart.bind(this, selectedProduct.id)}>Add to Cart</button>
-            <Link to="/cart">
-              <button className="product-view-btns">Review Cart</button>
-            </Link>
+            <h2>{selectedProduct.name}</h2>
+            <p>{selectedProduct.description}</p>
+
+            <div className="row">
+              <div className="column-2">
+                <span>{selectedProduct.price}</span>
+              </div>
+              <div className="column-2">
+                <button className="product-view-btns" onClick={this.handleAddItemToCart.bind(this, selectedProduct.id)}>Add to Cart</button>
+                <Link to="/cart">
+                  <button className="product-view-btns">Review Cart</button>
+                </Link>
+              </div>
+            </div>
+
           </div>
+
         </div>
-        <div className="similar-items">
+
+        <div className="similar-items column-2">
           {this.props.similarProducts.slice(0, 5).map((product) => {
-            return (<div key={product.id} className="column-5 catalog-tile">
+            return (<div key={product.id} className="column-5 similar-tile">
               <h4>{product.name}</h4>
-              <div className="product-thumbnail">
+              <div className="similar-thumbnail">
                 <Link to='#' onClick={this.handleNewItemClick.bind(this, product.id)}>
                   <img src={product.thumbnail} />
                 </Link>
